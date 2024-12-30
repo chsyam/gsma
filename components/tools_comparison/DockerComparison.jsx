b4013a13430e8e5a42a11c8fca96a3419b4f9959
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-12-30 19:06:05 +0530

    added tools comparison

--- components/tools_comparison/DockerComparison.jsx
+++ components/tools_comparison/DockerComparison.jsx
@@ -1,9 +1,52 @@
-export default function DockerComaparison({ dockerDetails }) {
-    console.log(dockerDetails);
-    
+import { Highcharts } from 'highcharts';
+import { Box, Divider, Modal } from "@mui/material";
+import Docker_SVG from "../icons/tools_svg/docker";
+import styles from "./ToolsComparison.module.css";
+import { useEffect, useState } from "react";
+import { X } from "lucide-react";
+import HighchartsReact from "highcharts-react-official";
+
+export default function DockerComaparison({ dockerDetails, popupData, setPopupData, setOpen }) {
+    const getEnergyUsageClass = (value) => {
+        switch (value) {
+            case "High":
+                return styles.danger;
+            case "Low":
+                return styles.success;
+            default:
+                return "";
+        }
+    }
+
+    const handleToolClick = (tool) => {
+        setPopupData({ ...tool });
+        setOpen(true);
+    }
+
+    useEffect(() => {
+        console.log(popupData);
+    }, [popupData])
+
     return (
-        <div>
-            Docker
+        <div className="flex justify-left gap-5 flex-wrap items-center mt-6">
+            {
+                dockerDetails?.map((tool, ind) => {
+                    return (
+                        <div onClick={() => handleToolClick(tool)} key={ind} className="bg-white min-w-[300px] rounded-md py-6 px-6 shadow-lg cursor-pointer">
+                            <div className={styles.title}>
+                                <Docker_SVG />{tool?.title}
+                            </div>
+                            <div className={styles.energyUsage}>
+                                <div>Energy Usage</div>
+                                <div className={getEnergyUsageClass(tool["basicMetrics"]["Energy Usage"]?.value)}
+                                >
+                                    {tool["basicMetrics"]["Energy Usage"]?.value}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
--- pages/dashboard/reports/tools-comparison/index.jsx
+++ pages/dashboard/reports/tools-comparison/index.jsx
@@ -1,50 +1,79 @@
+import { X } from "lucide-react";
 import { useState } from "react";
+import { ChevronDown } from "lucide-react";
 import { decrypt } from "./../../../api/auth/lib";
-import { ChevronDown, ExternalLink } from "lucide-react";
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, Modal } from "@mui/material";
 import toolsComparison from "./../../../../public/data/tools_comparison.json";
 import DockerComaparison from "../../../../components/tools_comparison/DockerComparison";
+import styles from "./../../../../components/tools_comparison/ToolsComparison.module.css";
+import Docker_SVG from "../../../../components/icons/tools_svg/docker";
 
-const tools_architecture = {
-    "Docker Build Comparison": ["docker"],
-    "Messaging between Java Microservices": ["sync", "async"],
-    "Energy efficient Workload Orchestrator": ["heft_ecws", "fcfs_ecws"],
-    "Python Web Frameworks": ["python_sync", "python_async"],
-    "Spring Boot Framework": ["spring"],
-    "Energy Consumption of Java I/O Libraries & Techniques": ["java_io"],
-    "Efficiency of Thread-Safe Collections in Java": ["thread_java"],
-    "Trade-Offs in Object-Relational Mapping(ORM) Framework": ["trade_offs"],
-    "Container Runtimes Across Various Scenarios": ["container_runtime"],
-    "Performance of Container Network Plugins(CNIs) in Virtualized Environment": ["cni_virtual"],
-    "Performance of Container Network Plugins(CNIs) in Physical Environment": ["cni_physical"],
-    "Performance of Container Network Plugins(CNIs) in Physical Environment": ["json_avro"],
-    "Serialization Efficiency : JSON vs Avro": ["json_avro"],
-    "Performance of API Data Formats: gRPC/Protobuf vs JSON": ["grpc_json"]
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
 };
 
-const ComponentsImporting = {
-    "docker": DockerComaparison,
-    "sync": DockerComaparison,
-    "async": DockerComaparison,
-    "heft_ecws": DockerComaparison,
-    "fcfs_ecws": DockerComaparison,
-    "python_sync": DockerComaparison,
-    "python_async": DockerComaparison,
-    "spring": DockerComaparison,
-    "java_io": DockerComaparison,
-    "thread_java": DockerComaparison,
-    "trade_offs": DockerComaparison,
-    "container_runtime": DockerComaparison,
-    "cni_virtual": DockerComaparison,
-    "cni_physical": DockerComaparison,
-    "json_avro": DockerComaparison,
-    "grpc_json": DockerComaparison
-}
+const tools_architecture = {
+    "Docker Build Comparison": {
+        "component": DockerComaparison, "data": [...toolsComparison["docker"]]
+    },
+    "Messaging between Java Microservices": {
+        "component": DockerComaparison, "data": [...toolsComparison["sync"], ...toolsComparison["async"]]
+    },
+    "Energy efficient Workload Orchestrator": {
+        "component": DockerComaparison, "data": [...toolsComparison["heft_ecws"], ...toolsComparison["fcfs_ecws"]]
+    },
+    "Python Web Frameworks": {
+        "component": DockerComaparison, "data": [...toolsComparison["python_sync"], ...toolsComparison["python_async"]]
+    },
+    "Spring Boot Framework": {
+        "component": DockerComaparison, "data": [...toolsComparison["spring"]]
+    },
+    "Energy Consumption of Java I/O Libraries & Techniques": {
+        "component": DockerComaparison, "data": [...toolsComparison["java_io"]]
+    },
+    "Efficiency of Thread-Safe Collections in Java": {
+        "component": DockerComaparison, "data": [...toolsComparison["thread_java"]]
+    },
+    "Trade-Offs in Object-Relational Mapping(ORM) Framework": {
+        "component": DockerComaparison, "data": [...toolsComparison["trade_offs"]]
+    },
+    "Container Runtimes Across Various Scenarios": {
+        "component": DockerComaparison, "data": [...toolsComparison["container_runtime"]]
+    },
+    "Performance of Container Network Plugins(CNIs) in Virtualized Environment": {
+        "component": DockerComaparison, "data": [...toolsComparison["cni_virtual"]]
+    },
+    "Performance of Container Network Plugins(CNIs) in Physical Environment": {
+        "component": DockerComaparison, "data": [...toolsComparison["cni_physical"]]
+    },
+    "Serialization Efficiency : JSON vs Avro": {
+        "component": DockerComaparison, "data": [...toolsComparison["json_avro"]]
+    },
+    "Performance of API Data Formats: gRPC/Protobuf vs JSON": {
+        "component": DockerComaparison, "data": [...toolsComparison["grpc_json"]]
+    },
+};
 
 export default function ToolsComparison() {
     console.log(toolsComparison);
     const [expandedPanels, setExpandedPanels] = useState({});
     const [toggleOperation, setToggleOperation] = useState("Expand");
+    const [popupData, setPopupData] = useState({});
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+        setPopupData({});
+    }
 
     const handleExpand = (panel) => {
         if (expandedPanels.hasOwnProperty(panel))
@@ -62,50 +91,116 @@ export default function ToolsComparison() {
         setExpandedPanels(temp);
     }
 
+    const getEnergyUsageClass = (value) => {
+        switch (value) {
+            case "High":
+                return styles.danger;
+            case "Low":
+                return styles.success;
+            default:
+                return "";
+        }
+    }
+
+    const callComponent = (Component, data) => {
+        return (
+            <Component
+                dockerDetails={data}
+                popupData={popupData}
+                setPopupData={setPopupData}
+                setOpen={setOpen}
+            />
+        )
+    }
+
     return (
-        <div>
-            <div>
-                {
-                    Object.keys(tools_architecture)?.map((area, index) => {
-                        return (
-                            <Accordion
-                                expanded={expandedPanels[index] ? true : false}
-                                key={index}
-                                onChange={() => handleExpand(index)}
-                                sx={{
-                                    backgroundColor: expandedPanels[index] ? '#F8F8F80D' : '#3E3E440D',
-                                    boxShadow: 'none',
-                                    margin: '4px 0',
-                                    borderTopLeftRadius: expandedPanels[index] ? '18px' : '4px',
-                                    borderTopRightRadius: expandedPanels[index] ? '18px' : '4px',
-                                    borderBottomLeftRadius: expandedPanels[index] ? '6px' : '4px',
-                                    borderBottomRightRadius: expandedPanels[index] ? '6px' : '4px'
-                                }}
+        <div className="px-[5%] py-8">
+            {
+                Object.keys(tools_architecture)?.map((area, index) => {
+                    return (
+                        <Accordion
+                            expanded={expandedPanels[index] ? true : false}
+                            key={index}
+                            onChange={() => handleExpand(index)}
+                            sx={{
+                                backgroundColor: expandedPanels[index] ? '#F8F8F80D' : '#3E3E440D',
+                                boxShadow: 'none',
+                                margin: '4px 0',
+                                borderTopLeftRadius: expandedPanels[index] ? '18px' : '4px',
+                                borderTopRightRadius: expandedPanels[index] ? '18px' : '4px',
+                                borderBottomLeftRadius: expandedPanels[index] ? '6px' : '4px',
+                                borderBottomRightRadius: expandedPanels[index] ? '6px' : '4px'
+                            }}
+                        >
+                            <AccordionSummary
+                                expandIcon={<ChevronDown color="#000" />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                                sx={{ fontSize: '14px', fontWeight: '500', backgroundColor: expandedPanels[index] ? '#549B79' : '3E3E440D', borderRadius: '4px' }}
                             >
-                                <AccordionSummary
-                                    expandIcon={<ChevronDown color="#000" />}
-                                    aria-controls="panel1-content"
-                                    id="panel1-header"
-                                    sx={{ fontSize: '14px', fontWeight: '500', backgroundColor: expandedPanels[index] ? '#549B79' : '3E3E440D', borderRadius: '4px' }}
+                                {area}
+                            </AccordionSummary>
+                            <AccordionDetails sx={{ backgroundColor: '#F8F8F80D' }}>
+                                <div>
+                                    {
+                                        callComponent(tools_architecture[area]?.component, tools_architecture[area]?.data)
+                                    }
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                    );
+                })
+            }
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...style, width: '60%' }}>
+                    <div className="flex justify-between">
+                        <div className={styles.title}>
+                            <Docker_SVG />
+                            {popupData?.title}
+                        </div>
+                        <div className="p-4 rounded-full bg-gray-100 cursor-pointer hover:bg-gray-300"
+                            onClick={handleClose}
+                        >
+                            <X />
+                        </div>
+                    </div>
+                    <div className={styles.energyUsage}>
+                        <div>Energy Usage</div>
+                        {
+                            Object.keys(popupData).length !== 0 && (
+                                <div className={getEnergyUsageClass(popupData["basicMetrics"]["Energy Usage"]?.value)}
                                 >
-                                    {area}
-                                </AccordionSummary>
-                                <AccordionDetails sx={{ backgroundColor: '#F8F8F80D' }}>
-                                    <div>
-                                        {
-                                            tools_architecture[area]?.map((item, ind) => {
-                                                return (
-                                                    callComponent(ComponentsImporting[item], toolsComparison[item], index)
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                </AccordionDetails>
-                            </Accordion>
-                        );
-                    })
-                }
-            </div>
+                                    {popupData["basicMetrics"]["Energy Usage"]?.value}
+                                </div>
+                            )
+                        }
+                    </div>
+                    <Divider className="my-6" />
+                    <div>
+                        <span className="font-semibold text-xl">Detailed Metrics</span>
+                        {
+                            Object.keys(popupData).length !== 0 && (
+                                Object.keys(popupData?.detailedMetrics).map((key, i) => {
+                                    return (
+                                        <div className="flex justify-between items-center my-4 bg-[#f5f5f5] px-2 py-2 rounded-[4px] font-medium">
+                                            <div>{key}</div>
+                                            <div className="bg-[#dcf2fe] px-2 py-1 rounded-[4px] font-semibold">{popupData?.detailedMetrics[key]}</div>
+                                        </div>
+                                    );
+                                })
+                            )
+                        }
+                    </div>
+                    <Divider className="my-6" />
+                    <div>{popupData?.description}</div>
+                </Box>
+            </Modal>
         </div>
     );
 }
